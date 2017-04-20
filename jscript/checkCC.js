710945f985bfe99d32cc85121039dfd97c51ff53
MadmanOfSteel <syunatan.klein@gmail.com>
2017-04-20 17:46:12 -0400

    Update checkCC.js

--- jscript/checkCC.js
+++ jscript/checkCC.js
@@ -1,1 +1,28 @@
 
+/**
+ * Created by Sam on 4/20/2017.
+ */
+/** Credit to alexey**/
+
+function checkCC(number) {
+    var regex = new RegExp("^[0-9]{16}$");
+    if (!regex.test(number))
+        return false;
+
+    return luhnCheck(number);
+}
+
+function luhnCheck(val) {
+    var sum = 0;
+    for (var i = 0; i < val.length; i++) {
+        var intVal = parseInt(val.substr(i, 1));
+        if (i % 2 == 0) {
+            intVal *= 2;
+            if (intVal > 9) {
+                intVal = 1 + (intVal % 10);
+            }
+        }
+        sum += intVal;
+    }
+    return (sum % 10) == 0;
+}
