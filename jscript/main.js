317d418a306407a843b6145f0fc8594aba692391
Justin Voorhees <voorhees.justin1@gmail.com>
2017-04-20 18:58:17 -0400

    Mark-Up and Initial Parsing
    This is the beginning of the extensive code being added for the Search Function.

--- jscript/main.js
+++ jscript/main.js
@@ -1,1 +1,22 @@
+/**
+ * Created by jvoorhee on 4/20/17.
+ */
+
+var book_list = [];
+var cart = [];
+var book_DOM = [];
+var profile_DOM = [];
+
+$(document).ready(function(e) {
+    $.getJSON("Books.json", function(data) {
+        $.each(data, function(key, val) {
+            book_list.push(val);
+        });
+    })
+    
+    for (index in book_list) {
+        add_results(index);
+        add_profiles(index);
+    }
+});
 
